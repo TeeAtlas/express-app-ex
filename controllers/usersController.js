aeac73ddefd9bf1c963557b518bfdba22db9d00c
Tania Boterman <taniaboterman@gmail.com>
2024-01-30 17:13:11 +0100

    get user and users completed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,10 +12,18 @@ export const getUsers = async (req, res) => {
 };
 
 export const getUser = async (req, res) => {
+    const {id} = req.params;
     
-        try {
-    
-        } catch (error) {
-    
+    try {
+        const {rows} = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        if(rows.length === 0) {
+            res.status(404).json({message: `user ${id} not found`});
+        } else {
+            //this sets default status to 200 (success)
+            res.json(rows[0]);
         }
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
 };
